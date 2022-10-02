b9a67ca9f80516b8b717f8d37a47c1944bb5286e
Aditya Rana <21bcs050@nith.ac.in>
2022-10-02 08:18:51 -0700

    1. Live Data Implemented to Unit Activity 2. Screenshots and VideoRecording Disabled
    Upnext: Live Data to home page, Notification RV, Lock Pre-requisite units, live data to likes and comments in forum, Assignments list in Unit, Assignment list live data from lesson, assignments in Profile, Events in Profile, Search for zoom/GoogleMeet api and percentile/lb in tests. UI Changes,
    Unit Complete alert
    
    Remaining Task
    1. Paginate Leaderboard

--- app/src/main/java/com/quaser/edtechapp/app/MyApplication.java
+++ app/src/main/java/com/quaser/edtechapp/app/MyApplication.java
@@ -1,20 +1,54 @@
 package com.quaser.edtechapp.app;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
 public class MyApplication extends Application {
     private static RequestQueue mainRequestQueue;
-
+    private Context context;
     @Override
     public void onCreate() {
         super.onCreate();
+        context = getApplicationContext();
+        setupActivityListener();
         mainRequestQueue = Volley.newRequestQueue(getApplicationContext());
     }
 
     public static RequestQueue getMainRequestQueue() {
         return mainRequestQueue;
+    }
+
+    private void setupActivityListener() {
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);            }
+            @Override
+            public void onActivityStarted(Activity activity) {
+            }
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+            @Override
+            public void onActivityStopped(Activity activity) {
+            }
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+            }
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+            }
+        });
     }
 }
